a0d356cb48e7cc51906ca47f464cd48e4e7237ae
itzKv <kenkenku6@gmail.com>
2026-07-02 09:23:36 +0700

    add: Koin initializer

--- androidApp/src/main/kotlin/com/dnk/hsakmp/core/di/KoinInitializer.kt
+++ androidApp/src/main/kotlin/com/dnk/hsakmp/core/di/KoinInitializer.kt
@@ -1,5 +1,16 @@
 package com.dnk.hsakmp.core.di
 
-class KoinInitializer {
-    
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+object KoinInitializer {
+    fun start(application: Application) {
+        startKoin {
+            androidLogger()
+            androidContext(application)
+            modules(sharedModules)
+        }
+    }
 }
